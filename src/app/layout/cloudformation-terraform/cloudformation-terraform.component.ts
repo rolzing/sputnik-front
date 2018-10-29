33ffea92c8b04741775a8b75f744cc78e0996017
Richard Hern <ric.lohern@gmail.com>
2018-10-29 12:03:48 -0600

    Custom config plugins 2

--- src/app/layout/cloudformation-terraform/cloudformation-terraform.component.ts
+++ src/app/layout/cloudformation-terraform/cloudformation-terraform.component.ts
@@ -1,26 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import {  BackendClass } from '../../services/backend.service';
+import { BackendClass } from '../../services/backend.service';
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 
 
 
 @Component({
-	selector: 'app-cloudformation-terraform',
-	templateUrl: './cloudformation-terraform.component.html'
+  selector: 'app-cloudformation-terraform',
+  templateUrl: './cloudformation-terraform.component.html'
 })
 export class CloudFormationTerraform implements OnInit {
-	form: FormGroup;
+  form: FormGroup;
   showTransformation: false;
   cloudFormationJson: any;
 
-  constructor( fb: FormBuilder,
-              private BackendClass: BackendClass) {
+  constructor(fb: FormBuilder,
+    private BackendClass: BackendClass,
+    private http: HttpClient) {
     this.form = fb.group({
       file: ['', Validators.required]
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   onImport(event) {
     var file = event.srcElement.files[0];
@@ -36,8 +38,11 @@ export class CloudFormationTerraform implements OnInit {
     }
   }
 
-  sendToBackend(){
-    this.BackendClass.CloudFormationPost(this.form.value)
+  sendToBackend() {
+    //this.BackendClass.CloudFormationPost(this.form.value)
+    this.http.post('192.168.1.0/cf', this.form.value).subscribe((response: any) => {
+      console.log(response.status)
+    })
   }
   //TODO: Add services and change boolean to show result in page
 
